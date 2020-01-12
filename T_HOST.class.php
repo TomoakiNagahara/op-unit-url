789da355c395d0dc59acb1ff93e11a1d9728f411
Tomoaki macbook12i7 <tomoaki.nagahara@gmail.com>
2020-01-13 00:41:47 +0900

    New: T_HOST::Get(), Update(), Delete()

--- T_HOST.class.php
+++ T_HOST.class.php
@@ -43,9 +43,51 @@ class T_HOST extends TABLE
 
 	/** Get ai.
 	 *
+	 * @param  string  $host
+	 * @return integer $ai
 	 */
 	static function Ai($host)
 	{
 		return self::_Ai(self::table, 'host', $host);
+	}
+
+	/** Get record by condition.
+	 *
+	 * @param   array  $condition
+	 * @return  array  $record
+	 */
+	static function Get($condition)
+	{
+		return self::_Get(self::table, $condition);
+	}
+
+	/** Update
+	 *
+	 * @created  2019-06-14
+	 * @copied   2019-07-04 from T_URL
+	 * @param    integer    $ai
+	 * @param    array      $update
+	 * @return   integer    $number
+	 */
+	static function Update($ai, $update)
+	{
+		//	...
+		$config = [];
+		$config['table']   = self::table;
+		$config['limit']   = 1;
+		$config['set']     = $update;
+		$config['where'][] = " ai = $ai ";
+		return self::DB()->Update($config);
+	}
+
+	/** Delete
+	 *
+	 * @created  2019-09-06
+	 * @param    integer     $ai
+	 * @return   boolean     $io
+	 */
+	static function Delete($ai)
+	{
+		return self::_Delete(self::table, $ai);
 	}
 }
